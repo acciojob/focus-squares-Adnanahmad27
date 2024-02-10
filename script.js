d832bffb32f0147eb505168d0e6a1a885aec2231
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-02-10 23:07:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+const squares = [];
+for(let i=0 ; i<3 ; i++){
+	let id = i+1;
+	squares[i] = document.getElementById("square"+id);
+}
+for(let i=0 ; i<3 ; i++){
+	squares[i].addEventListener("mouseover", onMouse);
+	squares[i].addEventListener("mouseout", offMouse);
+}
+function toggleBgColor(e){
+	e.classList.toggle("coffee");
+	e.classList.toggle("lavender");
+}
+function onMouse(e){
+	for(let j=0 ; j<3 ; j++){
+		if(e.target!==squares[j]){
+			toggleBgColor(squares[j]);
+		}
+	}
+}
+function offMouse(e){
+	for(let j=0 ; j<3 ; j++){		
+		if(e.target!==squares[j]){
+			toggleBgColor(squares[j]);
+		}
+	}
+}
